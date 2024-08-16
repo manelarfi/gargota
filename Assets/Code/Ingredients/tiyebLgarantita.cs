55238d08111cf46ee3b6fcbb4c3df4558991795e
manelarfi <manelarfi@gmail.com>
2024-08-16 18:40:21 +0100

    adding the cat

--- Assets/Code/Ingredients/tiyebLgarantita.cs
+++ Assets/Code/Ingredients/tiyebLgarantita.cs
@@ -23,6 +23,7 @@ public class TiyebLgarantita : MonoBehaviour, Interactable
         if (hands.transform.childCount > 0 && hands.transform.GetChild(0).name == "sniwabGarantitaNC") {
             Debug.Log("Destroying object in hands");
             Destroy(hands.transform.GetChild(0).gameObject);
+            pickupSystem.isHolding = false;
             timer = Time.realtimeSinceStartup;
         } 
         else if (hands.transform.childCount == 0 && Time.realtimeSinceStartup - timer > cookingTime) {
